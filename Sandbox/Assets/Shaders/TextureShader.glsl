1f55adfba44fc6ec23057dc38a6206acb0b08079
TanyaSravan <tanyasravan79@gmail.com>
2024-08-13 01:34:57 +0530

    moved numTiles to vertex shader

--- Sandbox/Assets/Shaders/TextureShader.glsl
+++ Sandbox/Assets/Shaders/TextureShader.glsl
@@ -6,10 +6,12 @@ layout(location = 1) in vec2 TexCoord;
 uniform mat4 u_VP;
 uniform mat4 u_Transform;
 
+uniform float u_NumTiles;
+
 out vec2 v_TexCoord;
 
 void main() {
-	v_TexCoord = TexCoord;
+	v_TexCoord = TexCoord * u_NumTiles;
 	gl_Position = u_VP * u_Transform * position;
 };
 
@@ -22,8 +24,7 @@ in vec2 v_TexCoord;
 
 uniform sampler2D u_Texture;
 uniform vec4 u_Color;
-uniform float u_NumTiles;
 
 void main() {
-	color = texture(u_Texture, v_TexCoord * u_NumTiles) * u_Color;
+	color = texture(u_Texture, v_TexCoord) * u_Color;
 }
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -25,7 +25,7 @@ void Sandbox2D:: OnUpdate(Forge::Timestep time) {
 	Forge::Renderer2D::DrawQuad({ 0.5f,0.0f,0.0f }, glm::vec2(1.0f), { m_SquareColor, 1.0f });
 	Forge::Renderer2D::DrawQuad({ 1.5f,-0.5f,0.0f }, glm::vec2(0.5f, 0.8f), { 0.8f,0.2f,0.2f, 1.0f });
 
-	Forge::Renderer2D::DrawQuad({ -1.0f,0.5f,-0.1f }, glm::vec2(10.0f), m_Texture2D);
+	Forge::Renderer2D::DrawQuad({ -1.0f,0.5f,-0.1f }, glm::vec2(10.0f), m_Texture2D,10);
 
 	Forge::Renderer::EndScene();
 }
